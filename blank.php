396908992cc22b0acb9781f8a642965ae30657ac
asadutzaman <mdasadutzaman@gmail.com>
2019-06-19 16:33:09 +0600

    add blank page

---
+++ blank.php
@@ -0,0 +1,23 @@
+<?php include 'header.php'; ?>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+            Blank Page
+            <small>Version 2.0</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Blank Page</a></li>
+            <li class="active">Dashboard</li>
+        </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-md-6">test</div>
+        </div>
+    </section>
+
+</div>    
+<?php include 'footer.php'; ?>
