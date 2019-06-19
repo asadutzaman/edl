3341ae21e1be62d85ff3b1bb570fa0cfa957d29a
asadutzaman <mdasadutzaman@gmail.com>
2019-06-19 16:36:06 +0600

    test space

--- blank.php
+++ blank.php
@@ -15,7 +15,7 @@
     <!-- Main content -->
     <section class="content">
         <div class="row">
-            <div class="col-md-6">test</div>
+            <div class="col-md-6">test </div>
         </div>
     </section>
 
